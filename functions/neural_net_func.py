898207e20d5e181cb2f6079105c8667d36818ec8
Junting <59756098+junting-ren@users.noreply.github.com>
2022-12-12 10:51:44 -0800

    Update neural_net_func.py
    Smoothed prediction

--- functions/neural_net_func.py
+++ functions/neural_net_func.py
@@ -47,38 +47,6 @@ def val(X, y, model, loss_fn, device):
     return loss.item()
 
 
-# def nn_fit_predict(model, y, X, X_test, n_iter = 100, lr = 0.01, device = 'cpu', verbose = False, patience = 10, weight_decay = 0):
-#     loss_fn = nn.MSELoss()
-#     optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = weight_decay)
-#     # split the training data into 20% validation and 80% training
-#     #import pdb; pdb.set_trace()
-#     index = np.random.permutation(X.shape[0])
-#     split = X.shape[0]//5
-#     train_idx, val_idx = index[split:], index[:split]
-#     X_train = X[train_idx,:]
-#     y_train = y[train_idx]
-#     X_val = X[val_idx,:]
-#     y_val = y[val_idx]
-    
-#     min_loss_val = float('inf')
-#     patience_pass = 0
-#     mean_num = n_iter//10
-#     list_pred = [None]*mean_num
-#     for i in range(n_iter):
-#         loss_train = train(X_train, y_train, model, loss_fn, optimizer, device = device)
-#         loss_val = val(X_val, y_val, model, loss_fn, device = device)
-#         list_pred[i%mean_num] = model.forward(X_test.to(device)).squeeze(1).cpu().detach().numpy()
-#         if min_loss_val > loss_val:
-#             model_best = copy.deepcopy(model)
-#             list_pred_best = list_pred
-#         else:
-#             patience_pass += 1
-#         if patience_pass > patience:
-#             break
-#         if verbose:
-#             print(f'Loss is {loss} at iteration {i}')
-#     #import pdb; pdb.set_trace()
-#     return np.mean(list_pred_best, axis = 0)
 def nn_fit_predict(model, y, X, X_test, n_iter = 100, lr = 0.01, device = 'cpu', verbose = False, patience = 10, weight_decay = 0):
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = weight_decay)
@@ -94,15 +62,47 @@ def nn_fit_predict(model, y, X, X_test, n_iter = 100, lr = 0.01, device = 'cpu',
     
     min_loss_val = float('inf')
     patience_pass = 0
+    mean_num = n_iter//10
+    list_pred = [None]*mean_num
     for i in range(n_iter):
         loss_train = train(X_train, y_train, model, loss_fn, optimizer, device = device)
         loss_val = val(X_val, y_val, model, loss_fn, device = device)
+        list_pred[i%mean_num] = model.forward(X_test.to(device)).squeeze(1).cpu().detach().numpy()
         if min_loss_val > loss_val:
             model_best = copy.deepcopy(model)
+            list_pred_best = list_pred
         else:
             patience_pass += 1
         if patience_pass > patience:
             break
         if verbose:
             print(f'Loss is {loss} at iteration {i}')
-    return model_best.forward(X_test.to(device)).squeeze(1).cpu().detach().numpy()
+    #import pdb; pdb.set_trace()
+    return np.mean(list_pred_best, axis = 0)
+# def nn_fit_predict(model, y, X, X_test, n_iter = 100, lr = 0.01, device = 'cpu', verbose = False, patience = 10, weight_decay = 0):
+#     loss_fn = nn.MSELoss()
+#     optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = weight_decay)
+#     # split the training data into 20% validation and 80% training
+#     #import pdb; pdb.set_trace()
+#     index = np.random.permutation(X.shape[0])
+#     split = X.shape[0]//5
+#     train_idx, val_idx = index[split:], index[:split]
+#     X_train = X[train_idx,:]
+#     y_train = y[train_idx]
+#     X_val = X[val_idx,:]
+#     y_val = y[val_idx]
+    
+#     min_loss_val = float('inf')
+#     patience_pass = 0
+#     for i in range(n_iter):
+#         loss_train = train(X_train, y_train, model, loss_fn, optimizer, device = device)
+#         loss_val = val(X_val, y_val, model, loss_fn, device = device)
+#         if min_loss_val > loss_val:
+#             model_best = copy.deepcopy(model)
+#         else:
+#             patience_pass += 1
+#         if patience_pass > patience:
+#             break
+#         if verbose:
+#             print(f'Loss is {loss} at iteration {i}')
+#     return model_best.forward(X_test.to(device)).squeeze(1).cpu().detach().numpy()
