8fa6204e5a56b7ccb29e2b4b9e53ea3ec2458b2a
Junting <tim.rainbig@gmail.com>
2023-01-20 09:23:51 -0800

    Update main_linear.py
    Added option for ridge as well as Monte Carlo simulation

--- functions/main_linear.py
+++ functions/main_linear.py
@@ -11,37 +11,44 @@ import linear_sim_func
 
 today = date.today()
 date = today.strftime("%d%m%Y")
-file_name = 'sim_results_ridge'+date+'.csv'
+file_name = 'sim_results_ridge_test'+date+'.csv'
 if __name__ ==  '__main__': 
     df_total = pd.DataFrame()
     n_sim = 500
-    N_v = [50,150,250]
+    N_v = [150]
     N_test_v = [500]
     p_v = [160]
-    error_sd_v = [1,4]
-    L_v = [0.95]
+    error_sd_v = [1]
+    L_v = [0.9]
     level_v = [0.2]
     use_true_contour_v = [False]
-    n_boot_v = [2000]
-    ridge_v = [True]
+    n_boot_v = [2000,1000]
+    ridge_penal_v = [[1,0.5,1e-1, 1e-2, 1e-3, 1e-4]]
+    MC_v = [False, True]
+    smoothed_v = [False]
+    residual_boot_v = [False]
+    CV_boot_v = [True, False]
     df_result = []
     param_grid = {'N': N_v, 'N_test': N_test_v, 'p': p_v , 'error_sd': error_sd_v, 'L': L_v,
-                  'level': level_v,  'use_true_contour': use_true_contour_v, 'n_boot':n_boot_v,'ridge':ridge_v}
+                  'level': level_v,  'use_true_contour': use_true_contour_v, 'n_boot':n_boot_v,
+                  'ridge_penal':ridge_penal_v, 'MC':MC_v, 'smoothed':smoothed_v, 'residual_boot':residual_boot_v, 'CV_boot':CV_boot_v}
     param_grid = ParameterGrid(param_grid)
     start = time.time()
     for param in param_grid:
-        df = pd.DataFrame(param, index = [0])
+        para_name_dict = {k:str(v) for k, v in param.items()}
+        df = pd.DataFrame(para_name_dict, index = [0])
         para_df = pd.DataFrame(np.repeat(df.values,n_sim, axis=0))
+        para_df.columns = df.columns
         pool_obj = multiprocessing.Pool()
         cur_para = (n_sim*((param['N'], param['N_test'], param['p'], param['error_sd'], \
-                          param['L'],param['level'],param['use_true_contour'], param['n_boot'], param['ridge']),))
-        df_result.append(np.array(pool_obj.starmap(linear_sim_func.safe_sim_linear,cur_para)))
-        pool_obj.close()
-        pool_obj.join()
-        df_ = pd.DataFrame(np.concatenate(df_result, axis = 0), 
+                          param['L'],param['level'],param['use_true_contour'], param['n_boot'], 
+                            param['ridge_penal'], param['MC'], param['smoothed'], param['residual_boot'], param['CV_boot']),))
+        df_ = pd.DataFrame(np.array(pool_obj.starmap(linear_sim_func.safe_sim_linear,cur_para)), 
                      columns = ["contain","contain_scb","contain_CS_scb","Lower_bound",
                                 "Upper_bound", "L1", "L2", "U1", "U2", "points_in_e1", 
-                                "pre_mae", 'inner_points_num', 'outer_points_num' ,'true_set_points_num'])
+                                "pre_mae", 'pre_mse','inner_points_num', 'outer_points_num' ,'true_set_points_num'])
+        pool_obj.close()
+        pool_obj.join()
         df_ = pd.concat([para_df, df_], axis = 1)
         df_total = pd.concat([df_total,df_])
         df_total.to_csv(file_name, index = False)
