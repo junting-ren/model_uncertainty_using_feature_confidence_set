d067f08372fb752480e7c47d080d578042d64683
Junting <59756098+junting-ren@users.noreply.github.com>
2023-01-31 11:07:05 -0800

    Update main_linear.py
    Adaptation to centered G

--- functions/main_linear.py
+++ functions/main_linear.py
@@ -11,27 +11,29 @@ import linear_sim_func
 
 today = date.today()
 date = today.strftime("%d%m%Y")
-file_name = 'sim_results_ridge_test'+date+'.csv'
+file_name = 'sim_results_Ridge_true_contour'+date+'.csv'
 if __name__ ==  '__main__': 
     df_total = pd.DataFrame()
     n_sim = 500
-    N_v = [150]
+    N_v = [30]
     N_test_v = [500]
-    p_v = [160]
+    p_v = [50]
     error_sd_v = [1]
     L_v = [0.9]
     level_v = [0.2]
-    use_true_contour_v = [False]
-    n_boot_v = [2000,1000]
+    use_true_contour_v = [False,True]
+    n_boot_v = [2000]
     ridge_penal_v = [[1,0.5,1e-1, 1e-2, 1e-3, 1e-4]]
-    MC_v = [False, True]
+    #ridge_penal_v = [None]
+    MC_v = [False]
     smoothed_v = [False]
     residual_boot_v = [False]
-    CV_boot_v = [True, False]
+    CV_boot_v = [True]
+    center_G_v = [False, True]
     df_result = []
     param_grid = {'N': N_v, 'N_test': N_test_v, 'p': p_v , 'error_sd': error_sd_v, 'L': L_v,
                   'level': level_v,  'use_true_contour': use_true_contour_v, 'n_boot':n_boot_v,
-                  'ridge_penal':ridge_penal_v, 'MC':MC_v, 'smoothed':smoothed_v, 'residual_boot':residual_boot_v, 'CV_boot':CV_boot_v}
+                  'ridge_penal':ridge_penal_v, 'MC':MC_v, 'smoothed':smoothed_v, 'residual_boot':residual_boot_v, 'CV_boot':CV_boot_v, 'center_G':center_G_v}
     param_grid = ParameterGrid(param_grid)
     start = time.time()
     for param in param_grid:
@@ -42,8 +44,8 @@ if __name__ ==  '__main__':
         pool_obj = multiprocessing.Pool()
         cur_para = (n_sim*((param['N'], param['N_test'], param['p'], param['error_sd'], \
                           param['L'],param['level'],param['use_true_contour'], param['n_boot'], 
-                            param['ridge_penal'], param['MC'], param['smoothed'], param['residual_boot'], param['CV_boot']),))
-        df_ = pd.DataFrame(np.array(pool_obj.starmap(linear_sim_func.safe_sim_linear,cur_para)), 
+                            param['ridge_penal'], param['MC'], param['smoothed'], param['residual_boot'], param['CV_boot'], param['center_G']),))
+        df_ = pd.DataFrame(np.array(pool_obj.starmap(linear_sim_func.sim_linear,cur_para)), 
                      columns = ["contain","contain_scb","contain_CS_scb","Lower_bound",
                                 "Upper_bound", "L1", "L2", "U1", "U2", "points_in_e1", 
                                 "pre_mae", 'pre_mse','inner_points_num', 'outer_points_num' ,'true_set_points_num'])
