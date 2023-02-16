170c44b86bc9a2122eada715042b71754fd4d351
juntingren <tim.rainbig@gmail.com>
2023-02-15 20:10:48 -0800

    Update NN_sim_func.py
    Added a few option: MC, second_stage, center_G, center_pred

--- functions/NN_sim_func.py
+++ functions/NN_sim_func.py
@@ -28,8 +28,8 @@ def transform_X_poly(X):
     # square
     square_X = np.square(X)
     # sqrt root
-    root_X = np.sqrt(X)
-    X = np.concatenate((root_X, square_X, X), axis = 1)
+    #root_X = np.sqrt(X)
+    X = np.concatenate((square_X, X), axis = 1)
     return np.concatenate((np.ones((N, 1)), X), axis = 1)
 
 
@@ -77,30 +77,46 @@ def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, see
         y = torch.tensor(y).double()
         X = torch.tensor(X).double()
         X_test = torch.tensor(X_test).double()
-    return X, y, X_test, y_test, mean_test_true
+    return X, y, X_test, y_test, mean_test_true, beta
 
 def sim_NN(N, N_test, p, error_sd, transform_func, 
            h_sizes, out_size, n_iter = 100, lr = 0.01, device = 'cpu', patience = 10, weight_decay = 0,
-           L = 0.925, level= 0, use_true_contour = False, n_boot = 500, beta = None, X_test = None,  return_range = False, batchnorm_ind = False):
+           L = 0.925, level= 0, use_true_contour = False, n_boot = 500, MC = False, second_stage = False, center_G = False,center_pred = False, 
+           beta = None, X_test = None,  return_range = False, batchnorm_ind = False):
     '''Function for neural network simulation
     
     '''
     
-    X, y, X_test, y_test, mean_test_true = generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, error_sd = error_sd, if_tensor = True)
-    mean, se, mean_boot_l  = bootstrap_func.fit_bootstrap_NN(y, X, X_test, p, h_sizes, out_size,
-                                              n_boot = n_boot, n_iter = n_iter, lr = lr, 
-                                              device = device, patience = patience, weight_decay = weight_decay, batchnorm_ind = batchnorm_ind)
+    X, y, X_test, y_test, mean_test_true, beta= generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, error_sd = error_sd, if_tensor = True)
+    if MC:
+        mean_boot_l = []
+        for i in range(n_boot):
+            X_new, y_new, _, _,_ = generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, beta = beta, error_sd = error_sd, if_tensor = True)
+            model = neural_net_func.FF_neural_net(input_size = input_size, h_sizes = h_sizes, out_size = out_size, batchnorm_ind = False)
+            mean_boot_l.append(neural_net_func.nn_fit_predict(model, y_new, X_new, X_test, n_iter = n_iter, lr = lr, device = device,  
+                          patience = patience, weight_decay = weight_decay) )
+        mean_boot_l = np.array(mean_boot_l)
+        se = np.std(mean_boot_l, axis = 0)
+        model = neural_net_func.FF_neural_net(input_size = input_size, h_sizes = h_sizes, out_size = out_size, batchnorm_ind = False)
+        mean = neural_net_func.nn_fit_predict(model, y, X, X_test, n_iter = n_iter, lr = lr, device = device,  
+                          patience = patience, weight_decay = weight_decay)
+    else:
+        mean, se, mean_boot_l, mean_train = bootstrap_func.fit_bootstrap_NN(y, X, X_test, p, h_sizes, out_size,
+                                                  n_boot = n_boot, n_iter = n_iter, lr = lr, 
+                                                  device = device, patience = patience, weight_decay = weight_decay, batchnorm_ind = batchnorm_ind)
     mae = np.mean(np.abs(mean - mean_test_true))
     #import pdb; pdb.set_trace()
     if level is None:
         level = np.mean(mean)
-    _, L, U, contain, contain_scb, contain_CS_scb, L1, L2, U1, U2, n_points, range_v,inner_points_num,outer_points_num,true_set_points_num = confidence_set_func.prediction_confidence_set(L, level, mean, se, mean_boot_l, mean_test_true = mean_test_true, use_true_contour = use_true_contour)
+    G, mean, se =confidence_set_func.process_boot_samples(mean_boot_l, mean, se, mean_test_true, 
+                                      MC, second_stage, center_G, center_pred)
+    _, L, U, contain, contain_scb, contain_CS_scb, L1, L2, U1, U2, n_points, range_v,inner_points_num,outer_points_num,true_set_points_num = confidence_set_func.prediction_confidence_set(L, level, mean, se, G, mean_test_true = mean_test_true, use_true_contour = use_true_contour)
     p = X_test.shape[1]
     N_test = X_test.shape[0]
     if return_range:
-        return contain, N, N_test, p, error_sd, level, use_true_contour, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, range_v, inner_points_num,outer_points_num,true_set_points_num
+        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, range_v, inner_points_num,outer_points_num,true_set_points_num
     else:
-        return contain, N, N_test, p, error_sd, level, use_true_contour, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, inner_points_num,outer_points_num,true_set_points_num
+        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, inner_points_num,outer_points_num,true_set_points_num
     
 def safe_sim_NN(N, N_test, p, error_sd, transform_func, 
                 h_sizes, out_size, n_iter = 100, lr = 0.01, device = 'cpu', patience = 10, weight_decay = 0,
@@ -112,4 +128,4 @@ def safe_sim_NN(N, N_test, p, error_sd, transform_func,
                       L = L, level= level, use_true_contour = use_true_contour,
                       n_boot = n_boot, beta = beta, X_test = X_test,  return_range = return_range, batchnorm_ind = batchnorm_ind)
     except:
-        return -1, N, N_test, p, error_sd, level, use_true_contour, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1,-1,-1
+        return -1,  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1,-1,-1
