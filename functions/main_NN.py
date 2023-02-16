d0362322e80cb5c7d7e9f9ee9d072e070b81e0f4
juntingren <tim.rainbig@gmail.com>
2023-02-15 20:08:02 -0800

    Update main_NN.py
    Updated simulation setup as well as the output dataframe format

--- functions/main_NN.py
+++ functions/main_NN.py
@@ -13,13 +13,13 @@ date = today.strftime("%d%m%Y")
 file_name = 'sim_NN_results_no_penal_mean_predict'+date+'.csv'
 if __name__ ==  '__main__': 
     df_total = pd.DataFrame()
-    n_sim = 100
-    N_v = [50,100,200]
+    n_sim = 1
+    N_v = [100]
     N_test_v = [500]
-    p_v = [2,4]
+    p_v = [1]
     error_sd_v = [1]
-    transform_func_v = [NN_sim_func.transform_X]
-    h_sizes_v = [[10,50,10]]
+    transform_func_v = [NN_sim_func.transform_X_poly]
+    h_sizes_v = [[5,5]]
     out_size_v = [1]
     n_iter_v = [100]
     lr_v = [0.01]
@@ -30,32 +30,39 @@ if __name__ ==  '__main__':
     level_v = [None]
     use_true_contour_v = [False]
     n_boot_v = [500]
+    MC_v = [False]
+    second_stage_v = [True, False]
+    center_G_v = [True, False]
+    center_pred_v = [True,False]
     df_result = []
     param_grid = {'N': N_v, 'N_test': N_test_v, 'p': p_v , 'error_sd': error_sd_v, 
                   'transform_func':transform_func_v, 'h_sizes':h_sizes_v,
                   'out_size':out_size_v, 'n_iter':n_iter_v, 'lr':lr_v, 'device':device_v,
                   'patience':patience_v, 'weight_decay': weight_decay_v,
-                  'L': L_v,'level': level_v,  'use_true_contour': use_true_contour_v, 'n_boot':n_boot_v}
+                  'L': L_v,'level': level_v,  'use_true_contour': use_true_contour_v, 'n_boot':n_boot_v,
+                  'MC':MC_v, 'second_stage':second_stage_v, 'center_G':center_G_v, 'center_pred':center_pred_v}
     param_grid = ParameterGrid(param_grid)
     start = time.time()
     for param in param_grid:
+        para_name_dict = {k:str(v) for k, v in param.items()}
+        df = pd.DataFrame(para_name_dict, index = [0])
+        para_df = pd.DataFrame(np.repeat(df.values,n_sim, axis=0))
+        para_df.columns = df.columns
         ctx = torch.multiprocessing.get_context('spawn')
         pool_obj = ctx.Pool()
         cur_para = (n_sim*((param['N'], param['N_test'], param['p'], param['error_sd'], \
                             param['transform_func'], param['h_sizes'],param['out_size'],
                             param['n_iter'],param['lr'],param['device'],param['patience'],param['weight_decay'],
-                          param['L'],param['level'],param['use_true_contour'], param['n_boot']),))
-        df_result.append(np.array(pool_obj.starmap(NN_sim_func.safe_sim_NN,cur_para)))
-        pool_obj.close()
-        pool_obj.join()
-        net_parameters = 'hidden='+str(param['h_sizes'])+'_n_iter='+str(param['n_iter'])+'_lr='+str(param['lr'])+'_weight_decay='+str(param['weight_decay'])
-        df_ = pd.DataFrame(np.concatenate(df_result, axis = 0), 
-                     columns = ["contain", "N", "N_test",
-                                "p", "error_sd", "level", "use_true_contour",
-                                "contain_scb","contain_CS_scb","Lower_bound",
+                          param['L'],param['level'],param['use_true_contour'], param['n_boot'], 
+                           param['MC'],param['second_stage'],param['center_G'],param['center_pred']
+                           ),))
+        df_ = pd.DataFrame(np.array(pool_obj.starmap(NN_sim_func.sim_NN,cur_para)), 
+                     columns = ["contain", "contain_scb","contain_CS_scb","Lower_bound",
                                 "Upper_bound", "L1", "L2", "U1", "U2", "points_in_e1", 
                                 "pre_mae", 'inner_points_num', 'outer_points_num' ,'true_set_points_num'])
-        df_ = pd.concat([pd.DataFrame([net_parameters]*df_.shape[0]), df_], axis = 1)
+        pool_obj.close()
+        pool_obj.join()
+        df_ = pd.concat([para_df, df_], axis = 1)
         df_total = pd.concat([df_total,df_])
         df_total.to_csv(file_name, index = False)
     end = time.time()
