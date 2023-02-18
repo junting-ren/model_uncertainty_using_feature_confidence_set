bf48e5e0add03b417445655e81e70567d277bfee
juntingren <tim.rainbig@gmail.com>
2023-02-17 17:43:57 -0800

    Update NN_sim_func.py
    Added the option to generate data from a uniform distribution

--- functions/NN_sim_func.py
+++ functions/NN_sim_func.py
@@ -33,7 +33,7 @@ def transform_X_poly(X):
     return np.concatenate((np.ones((N, 1)), X), axis = 1)
 
 
-def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, seed = None, X = None, X_test = None, if_tensor = False):
+def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, seed = None, X = None, X_test = None, if_tensor = False, uniform_range = None):
     '''Generate neural network data
     
     Parameters:
@@ -49,7 +49,7 @@ def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, see
     X: the training design matrix, defaults to None
     X_test: the testing design matrix, defaults to None
     if_tensor: whether to transform the return values to tensor
-    
+    uniform_range: if None, generate from N(0,1) else it must a list with two numbers indicating the low and high of uniform distribution
     Returns:
     -----------------------------------
     A tuple containing the following:
@@ -62,14 +62,21 @@ def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, see
     rng = np.random.default_rng(seed)
     # Training dataset 
     if X is None:
-        X = rng.standard_normal(size = (N,p))
+        if uniform_range is None:
+            X = rng.standard_normal(size = (N,p))
+        else:
+            X = rng.uniform(uniform_range[0], uniform_range[1], size = (N,p))
     X_train_transformed = transform_func(X)
     if beta is None:
         beta = rng.standard_normal(size = X_train_transformed.shape[1])
     y = X_train_transformed @ beta + rng.normal(loc = 0,scale = error_sd, size = (N,))
     # Test dataset
+    #import pdb; pdb.set_trace()
     if X_test is None:
-        X_test = rng.standard_normal(size = (N_test,p))
+        if uniform_range is None:
+            X_test = rng.standard_normal(size = (N_test,p))
+        else:
+            X_test = rng.uniform(uniform_range[0], uniform_range[1], size = (N_test,p))
     X_test_transformed = transform_func(X_test)
     mean_test_true = X_test_transformed @ beta
     y_test = mean_test_true + rng.normal(loc = 0,scale = error_sd, size = (N_test,))
@@ -81,51 +88,57 @@ def generate_NN_data(N, N_test, p,  transform_func, beta=None, error_sd = 3, see
 
 def sim_NN(N, N_test, p, error_sd, transform_func, 
            h_sizes, out_size, n_iter = 100, lr = 0.01, device = 'cpu', patience = 10, weight_decay = 0,
-           L = 0.925, level= 0, use_true_contour = False, n_boot = 500, MC = False, second_stage = False, center_G = False,center_pred = False, 
+           L = 0.925, level= 0, use_true_contour = False, n_boot = 500, MC = False, second_stage = False, 
+           center_G = False,center_pred = False, mean_num = 1, uniform_range = None, 
            beta = None, X_test = None,  return_range = False, batchnorm_ind = False):
     '''Function for neural network simulation
     
     '''
     
-    X, y, X_test, y_test, mean_test_true, beta= generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, error_sd = error_sd, if_tensor = True)
+    X, y, X_test, y_test, mean_test_true, beta= generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func,beta = beta,error_sd = error_sd, if_tensor = True, uniform_range = uniform_range)
     if MC:
         mean_boot_l = []
         for i in range(n_boot):
-            X_new, y_new, _, _,_ = generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, beta = beta, error_sd = error_sd, if_tensor = True)
+            X_new, y_new, _, _,_ = generate_NN_data(N = N, N_test = N_test, p = p, transform_func = transform_func, beta = beta, error_sd = error_sd, if_tensor = True, uniform_range = uniform_range)
             model = neural_net_func.FF_neural_net(input_size = input_size, h_sizes = h_sizes, out_size = out_size, batchnorm_ind = False)
             mean_boot_l.append(neural_net_func.nn_fit_predict(model, y_new, X_new, X_test, n_iter = n_iter, lr = lr, device = device,  
                           patience = patience, weight_decay = weight_decay) )
         mean_boot_l = np.array(mean_boot_l)
         se = np.std(mean_boot_l, axis = 0)
-        model = neural_net_func.FF_neural_net(input_size = input_size, h_sizes = h_sizes, out_size = out_size, batchnorm_ind = False)
+        model = neural_net_func.FF_neural_net(input_size = input_size, h_sizes = h_sizes, out_size = out_size, batchnorm_ind = False, mean_num = mean_num)
         mean = neural_net_func.nn_fit_predict(model, y, X, X_test, n_iter = n_iter, lr = lr, device = device,  
                           patience = patience, weight_decay = weight_decay)
     else:
         mean, se, mean_boot_l, mean_train = bootstrap_func.fit_bootstrap_NN(y, X, X_test, p, h_sizes, out_size,
                                                   n_boot = n_boot, n_iter = n_iter, lr = lr, 
-                                                  device = device, patience = patience, weight_decay = weight_decay, batchnorm_ind = batchnorm_ind)
+                                                  device = device, patience = patience, weight_decay = weight_decay, batchnorm_ind = batchnorm_ind, mean_num = mean_num)
     mae = np.mean(np.abs(mean - mean_test_true))
+    mse = np.mean(np.square(mean - mean_test_true))
     #import pdb; pdb.set_trace()
-    if level is None:
-        level = np.mean(mean)
     G, mean, se =confidence_set_func.process_boot_samples(mean_boot_l, mean, se, mean_test_true, 
                                       MC, second_stage, center_G, center_pred)
+    if level is None:
+        level = np.mean(mean)
     _, L, U, contain, contain_scb, contain_CS_scb, L1, L2, U1, U2, n_points, range_v,inner_points_num,outer_points_num,true_set_points_num = confidence_set_func.prediction_confidence_set(L, level, mean, se, G, mean_test_true = mean_test_true, use_true_contour = use_true_contour)
     p = X_test.shape[1]
     N_test = X_test.shape[0]
     if return_range:
-        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, range_v, inner_points_num,outer_points_num,true_set_points_num
+        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, mse, range_v, inner_points_num,outer_points_num,true_set_points_num
     else:
-        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, inner_points_num,outer_points_num,true_set_points_num
+        return contain, contain_scb, contain_CS_scb, L, U, L1, L2, U1, U2, n_points, mae, mse, inner_points_num,outer_points_num,true_set_points_num
     
 def safe_sim_NN(N, N_test, p, error_sd, transform_func, 
-                h_sizes, out_size, n_iter = 100, lr = 0.01, device = 'cpu', patience = 10, weight_decay = 0,
-                L = 0.925, level= None, use_true_contour = False, n_boot = 500, beta = None, X_test = None,  return_range = False,batchnorm_ind = False):
+           h_sizes, out_size, n_iter = 100, lr = 0.01, device = 'cpu', patience = 10, weight_decay = 0,
+           L = 0.925, level= 0, use_true_contour = False, n_boot = 500, MC = False, second_stage = False, 
+           center_G = False,center_pred = False, mean_num = 1, uniform_range = None, 
+           beta = None, X_test = None,  return_range = False, batchnorm_ind = False):
     try:
         return sim_NN(N, N_test, p, error_sd, transform_func, 
                       h_sizes, out_size, n_iter = n_iter, lr = lr, 
                       device = device, patience = patience, weight_decay = weight_decay,
                       L = L, level= level, use_true_contour = use_true_contour,
-                      n_boot = n_boot, beta = beta, X_test = X_test,  return_range = return_range, batchnorm_ind = batchnorm_ind)
+                      n_boot = n_boot,  MC = MC, second_stage = second_stage, 
+                       center_G = center_G,center_pred = center_pred, mean_num = mean_num, uniform_range = uniform_range, 
+                      beta = beta, X_test = X_test,  return_range = return_range, batchnorm_ind = batchnorm_ind)
     except:
-        return -1,  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1,-1,-1
+        return -1,  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1,-1,-1, -1
