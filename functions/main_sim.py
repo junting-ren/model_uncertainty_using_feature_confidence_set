e7ab28e35aa45bffe0debede655c4eddf8a313f8
Junting <tim.rainbig@gmail.com>
2024-07-18 21:41:39 -0400

    Added logistic regression simulation

--- functions/main_sim.py
+++ functions/main_sim.py
@@ -15,27 +15,37 @@ from models import NueralNet, logistic_regression
 
 today = date.today()
 date = today.strftime("%d%m%Y")
-file_name = 'linear_regression_'+date+'.csv'
-folder_path = './linear_result'
-file_name = os.path.join(folder_path, file_name)
+model_type = "linear_regression"
 if __name__ ==  '__main__': 
     df_total = pd.DataFrame()
     n_sim = 500
+    n_boot_v = [300]
     L_v = [0.9, 0.6]
-    level_v = [2]
-    models_l_v = [[LinearRegression]]
-    models_kwargs_l_v = [[{}]]
     N_v = [100, 200, 400, 800]
     N_test_v = [500]
     p_v = [3, 6, 10]
-    error_sd_v = [1]
     data_sim_func_v = [generate_sim_data]
-    data_kwargs_v = [{'uniform_range_x':[-2,2]}]
     center_G_v = [True]
     center_pred_v = [False]
     use_true_contour_v = [False]
-    n_boot_v = [200]
     df_result = []
+    error_sd_v = [1]
+    if model_type == "logistic_regression":
+        level_v = [0.5]
+        models_l_v = [[logistic_regression]]
+        models_kwargs_l_v = [[{}]]
+        data_kwargs_v = [{'uniform_range_x':[-2,2], "binary": True, "uniform_range_beta": [1,3]}]
+        folder_path = './linear_result'
+    elif model_type == "linear_regression":
+        level_v = [0]
+        models_l_v = [[LinearRegression]]
+        models_kwargs_l_v = [[{}]]
+        data_kwargs_v = [{'uniform_range_x':[-2,2]}]
+        folder_path = './linear_result'
+    file_name = model_type+'_'+date+'.csv'
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    file_name = os.path.join(folder_path, file_name)
     param_grid = {'L': L_v, 'level': level_v, 'models_l': models_l_v,'model_kwargs_l':models_kwargs_l_v,
                   'N': N_v, 'N_test': N_test_v, 'p': p_v , 'error_sd': error_sd_v, 
                   'data_sim_func':data_sim_func_v, 'data_kwargs':data_kwargs_v,
@@ -44,6 +54,7 @@ if __name__ ==  '__main__':
     param_grid = ParameterGrid(param_grid)
     start = time.time()
     for param in param_grid:
+        #sim_CS_wrapper(param)
         para_name_dict = {k:str(v) for k, v in param.items()}
         df = pd.DataFrame(para_name_dict, index = [0])
         ctx = torch.multiprocessing.get_context('spawn')
